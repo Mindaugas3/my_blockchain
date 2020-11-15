24810161df05698e55f944106bf8b5071ec174df
MindaugasK <mindaugas.krup@gmail.com>
2020-11-15 12:32:58 +0200

    +

---
+++ Launch_Parallel.cmd
@@ -0,0 +1,2 @@
+mpiexec -debug 3 -n 4 cmake-build-debug\blockchn.exe
+pause
--- main.cpp
+++ main.cpp
@@ -22,6 +22,7 @@ using namespace std;
 
 int main(int argc, char** argv) {
 
+
     MPI_Init(NULL, NULL);
 
     int procid;
